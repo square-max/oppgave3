e3e50f35b01900a276806bde77de246ae5275f85
peder august winge <54266739+square-max@users.noreply.github.com>
2019-11-28 14:17:05 +0100

    Updates

---
+++ precache-manifest.edb69fba4b35e2e4d6b343a6413a1ac9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b866b86307e85521dba7aa098754ec",
+    "url": "/oppgave3/index.html"
+  },
+  {
+    "revision": "d817a54d8585aa6b1fc6",
+    "url": "/oppgave3/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "586774eea3351b0c066b",
+    "url": "/oppgave3/static/js/2.2db8ca07.chunk.js"
+  },
+  {
+    "revision": "d817a54d8585aa6b1fc6",
+    "url": "/oppgave3/static/js/main.c812f3c1.chunk.js"
+  },
+  {
+    "revision": "3e0dc9d1b89a58c9b475",
+    "url": "/oppgave3/static/js/runtime-main.f4448b1d.js"
+  }
+]);
